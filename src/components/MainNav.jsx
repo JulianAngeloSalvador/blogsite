5e246eb44182ad141a7dd55dfaa1f7429dbcfd5f
annaangelaavelino <annaangelaavelino@gmail.com>
2023-12-20 19:11:10 +0800

    modified navbar

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,3 +1,19 @@
 export default function MainNav() {
-  return "Navbar";
+  return (
+    <nav className="bg-yellow-100 p-2 flex justify-between px-5 items-center">
+      <h4>PET CODE</h4>
+      <ul className="flex gap-6">
+        <li>Home</li>
+        <li>About</li>
+        <li>Search</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="flex gap-3">
+        <button className="auth-buttons bg-slate-50 rounded-full py-0.5 px-2">
+          Sign Up
+        </button>
+        <button className="auth-buttons">Login</button>
+      </div>
+    </nav>
+  );
 }
